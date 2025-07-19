ffd3d88176d0c9dc6cb14d7f5cf8caa8e4b190a4
kobot <164139791+AnhEmTu@users.noreply.github.com>
2025-07-19 10:02:49 +0700

    Add files via upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bypass seotrieuview (Tool)
+// @namespace    http://tampermonkey.net/
+// @version      Beta
+// @description  Auto vượt seotrieuview không cần chạm tay
+// @author       By R2lx
+// @require      https://raw.githubusercontent.com/AnhEmTu/Miley-/refs/heads/main/untitled.js
+// @match        https://seotrieuview.com/*
+// @grant        none
+// @icon         
+// ==/UserScript==
